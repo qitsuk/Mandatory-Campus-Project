6630b3c382b2a5d5c9c76ecf09a52e8fdf980d59
ClethK <Cecilie.l.karlsson@gmail.com>
2018-04-16 12:45:56 +0200

    database users

--- Database/new_campus.sql
+++ Database/new_campus.sql
@@ -12,3 +12,19 @@ CREATE TABLE CampusSchedule (
     Period INT(3),
 	Class VARCHAR(255)
 );
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `LastName` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `Username` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `Email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `PhoneNumber` int(8) NOT NULL,
+  `Address` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `Zip` int(4) NOT NULL,
+  `City` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `Password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `IsAdmin` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `IsAdmin` (`IsAdmin`)
+) 
