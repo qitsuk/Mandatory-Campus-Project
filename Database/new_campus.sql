9da1b0cc1aaf2f37d1c46c1a88403fbcf0c2e423
ClethK <Cecilie.l.karlsson@gmail.com>
2018-04-16 14:19:40 +0200

    database input

--- Database/new_campus.sql
+++ Database/new_campus.sql
@@ -1,20 +1,113 @@
-CREATE TABLE CampusRoom (
-	ID INT PRIMARY KEY AUTO_INCREMENT,
-    Name INT(6)
-);
-CREATE TABLE CampusDay (
-	WeekDay VARCHAR(10),
-    RoomNumber INT(6),
-	Period INT(3)
-);
-CREATE TABLE CampusSchedule (
-	WeekDay VARCHAR(10),
-    Period INT(3),
-	Subject VARCHAR(255), 
-    Class VARCHAR(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 16, 2018 at 12:18 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
-CREATE TABLE `users` (
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `new_campus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campusday`
+--
+
+DROP TABLE IF EXISTS `campusday`;
+CREATE TABLE IF NOT EXISTS `campusday` (
+  `WeekDay` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `RoomNumber` int(6) DEFAULT NULL,
+  `Period` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `campusday`
+--
+
+INSERT INTO `campusday` (`WeekDay`, `RoomNumber`, `Period`) VALUES
+('Monday', 105, NULL),
+('Tuesday', 106, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campusroom`
+--
+
+DROP TABLE IF EXISTS `campusroom`;
+CREATE TABLE IF NOT EXISTS `campusroom` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` int(6) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=107 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `campusroom`
+--
+
+INSERT INTO `campusroom` (`ID`, `Name`) VALUES
+(105, 105),
+(106, 106),
+(104, 104);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campusschedule`
+--
+
+DROP TABLE IF EXISTS `campusschedule`;
+CREATE TABLE IF NOT EXISTS `campusschedule` (
+  `WeekDay` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Period` int(3) DEFAULT NULL,
+  `Subject` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Class` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `campusschedule`
+--
+
+INSERT INTO `campusschedule` (`WeekDay`, `Period`, `Subject`, `Class`) VALUES
+('Monday', NULL, 'Php', 'ro17wd2u2-2u'),
+('Tuesday', NULL, 'VR/AR', 'ro17wd2u2-2u');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `period`
+--
+
+DROP TABLE IF EXISTS `period`;
+CREATE TABLE IF NOT EXISTS `period` (
+  `Class` int(11) NOT NULL,
+  `Subject` int(11) NOT NULL,
+  `Room` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `LastName` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
@@ -28,4 +121,18 @@ CREATE TABLE `users` (
   `IsAdmin` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `IsAdmin` (`IsAdmin`)
-) 
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `FirstName`, `LastName`, `Username`, `Email`, `PhoneNumber`, `Address`, `Zip`, `City`, `Password`, `IsAdmin`) VALUES
+(1, 'Cecilie', 'Karlsson', 'Theceka', 'cecilie.l.karlsson@gmail.com', 12345678, 'testvej 1', 4700, 'næstved', '1234', 1),
+(2, 'Kasper', 'Andreasen', 'qitsuk', 'test@test.com', 12345678, 'testvej 2', 1234, 'Præstø', '1324', 1),
+(3, 'Christoffer', 'Eriksen', 'McBruce', 'testing@test.com', 12345678, 'testvej 3', 1234, 'Nykøbing F', '1234', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
