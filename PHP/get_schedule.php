30bb2c3efdccc6945132bf2a673254f8b81ae4ba
qiTsuk <kasper.andreasen@gmail.com>
2018-04-20 18:06:07 +0200

    Did a little styling. Any help would be appreciated. Or else this is what will be handed in!

--- PHP/get_schedule.php
+++ PHP/get_schedule.php
@@ -1,45 +1,39 @@
 <?php
 include_once "dbfactory.php";
-$weekday = date('N');
+$tempWeekday = date('N');
+switch ($tempWeekday) {
+    case 1:
+        $weekday = 'Monday';
+        break;
+    case 2:
+        $weekday = 'Tuesday';
+        break;
+    case 3:
+        $weekday = 'Wednesday';
+        break;
+    case 4:
+        $weekday = 'Thursday';
+        break;
+    case 5:
+        $weekday = 'Friday';
+        break;
+    default:
+        $weekday = 'Not a schoolday!';
+        break;
+}
 //echo "$weekday - Is this the weekday?";
 if (isset($_GET['data'])) {
     $roomNumber = $_GET['data'];
-    $q = "SELECT * FROM CampusSchedule WHERE ClassRoom=$roomNumber AND Weekday=$weekday ORDER BY Period ASC;";
+    $q = "SELECT * FROM CampusSchedule WHERE ClassRoom=$roomNumber AND Weekday='$weekday' ORDER BY Period ASC;";
 //    echo "<br>$q";
 } else {
-    $q = "SELECT * FROM CampusSchedule WHERE Weekday=$weekday ORDER BY Period ASC;";
+    $q = "SELECT * FROM CampusSchedule WHERE Weekday='$weekday' ORDER BY Period ASC;";
 //    echo "<br>$q";
 }
 $result = mysqli_query($dbc, $q);
 //echo "<br>Right here, is where it should have just established the connection to the db!<br>";
 $resultArray = array();
 while ($row = $result -> fetch_assoc()) {
-//    echo "<br>Does it even go into the loop?";
-//    if ($row['Weekday']) {
-//        $day = "";
-//        switch ($row['Weekday']) {
-//            case 1:
-//                $day = 'Monday';
-//                break;
-//            case 2:
-//                $day = 'Tuesday';
-//                break;
-//            case 3:
-//                $day = 'Wednesday';
-//                break;
-//            case 4:
-//                $day = 'Thursday';
-//                break;
-//            case 5:
-//                $day = 'Friday';
-//                break;
-//            default:
-//                $day = 'Not a schoolday!';
-//                break;
-//        }
-//        //array_push($resultArray, $day);
-//    } else {
-        array_push($resultArray, $row);
-//    }
+    array_push($resultArray, $row);
 }
 echo json_encode($resultArray);
